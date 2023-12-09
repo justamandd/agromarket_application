1b0bf27c3544edc058a649e01f8b7e15cc8338a7
Vitor Justamand <justamandd@gmail.com>
2023-12-08 22:43:58 -0300

    feature(routes): order

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,10 @@ const routes = express.Router();
 import customerRouter from "./customer.routes";
 import farmRouter from "./farm.routes";
 import productRouter from "./product.routes";
+import orderRoutes from "./order.routes";
 
 routes.use('/customer', customerRouter);
 routes.use('/farm', farmRouter);
 routes.use('/product', productRouter)
-
+routes.use('/order', orderRoutes)
 export default routes;
