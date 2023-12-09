e73682a0a4b5b4f5875c09bac0c942a828867cbe
Vitor Justamand <justamandd@gmail.com>
2023-12-08 22:44:44 -0300

    fix(server.ts): dotenv added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from "express";
 import cors from 'cors';
 import routes from "./routes";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 8080;
@@ -10,7 +13,7 @@ app.use(cors());
 app.use(routes)
 
 app.get("/", async (req: Request, res: Response) => {
-    res.send("Test");
+    res.send("oi");
 })
 
 app.listen(port, () => console.log(`The api is running on http://localhost:${port}/`));
